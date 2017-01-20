b8dd528b2b986afa659209ee43635d1e9b226cfa
petermmm <p.matkovski@gmail.com>
2017-01-20 18:42:56 +0100

    init3

--- README.md
+++ README.md
@@ -1,21 +1,26 @@
 # ABOUT #
 
-Antiscrapper is Apache2 CGI script serving static content randomizing HTTP attributes:
+Antiscrapper is  a proof-of-concept script, using mitmproxy, that can obfuscate HTML code to mitigate basic web scrapping techniques.
 
+Obfuscation is divided into modules:
 * rename of variables
 * randomize HTTP structure with minimal impact on design
 * introduce sinkholes for scrap engines
+* plain text obfuscation
 
 ## Technology ##
 
-** Apache2 **
-* Modifying static content using a CGI script
-* http://aarvik.dk/how-to-setup-python-cgi-with-apache2/
-* https://httpd.apache.org/docs/2.4/handler.html
+** mitmproxy **
+
+* (mitmproxy.org)[https://mitmproxy.org/]
+* (mimtproxy Inline Scripts)[ https://mitmproxy.org/doc/scripting/inlinescripts.html] 
 
 ** Python3 **
-* http://www.python-course.eu/dynamic_websites.php
-* http://anh.cs.luc.edu/python/hands-on/3.1/handsonHtml/dynamic.html
+
+* (python3 docs)[https://docs.python.org]
+
+** BeautifulSoup **
+
 
 # Usage #
 
@@ -23,16 +28,22 @@ Antiscrapper is Apache2 CGI script serving static content randomizing HTTP attri
 ## Install ##
 To run antiscrapper you need to install some dependencies.
 
+### python3.5 ###
 
-```$ apt-get install apache2 python3```
-```sudo apt-get install libapache2-mod-python```
- then 
+sudo apt-get install build-essential checkinstall
+
+install python 3.5!
+
+``` sudo apt-get install python3-pip python3-dev libffi-dev libssl-dev libtiff5-dev libjpeg8-dev zlib1g-dev libwebp-dev
+```sudo pip3 install --upgrade setuptools```
+```sudo pip3.5 install mitmproxy```
+
+
  
- https://www.digitalocean.com/community/tutorials/how-to-set-up-an-apache-mysql-and-python-lamp-server-without-frameworks-on-ubuntu-14-04
-
-
 
 ## Run ##
 
+To test it out, run mitmproxy with this script in regular mode:
 
+```mitmproxy -s "antiscraper.py"```
 
--- source/antiscraper.py
+++ source/antiscraper.py
@@ -1,60 +1,48 @@
 #!/usr/bin/env python3
 
-# -*- coding: UTF-8 -*-
+# Usage: mitmdump -s "antiscraper.py"
+# (this script works best with --anticache)
+
+from mitmproxy import ctx #for logging
+import sys, re
+from bs4 import BeautifulSoup
 
 
-import cgi
-import cgitb; cgitb.enable()
-import os, re
+class Injector:
+    #def __init__(self, iframe_url):
+    #    self.iframe_url = iframe_url
 
 
-def main(): # NEW except for the call to processInput
-    form = cgi.FieldStorage()      # standard cgi script lines to here!
+    def response(self, flow):
+        #if flow.request.host in self.iframe_url:
+        #    return
+        html = BeautifulSoup(flow.response.content, "html.parser")
+        if html.body:
 
-    '''
-    # use format of next two lines with YOUR names and default data
-    numStr1 = form.getfirst("x", "0") # get the form value associated with form
-                                   # name 'x'.  Use default "0" if there is none.
-    numStr2 = form.getfirst("y", "0") # similarly for name 'y'
-    contents = processInput(numStr1, numStr2)   # process input into a page
-    print(contents)
-    '''
+            ## insert iframe
+            #iframe = html.new_tag(
+            #    "iframe",
+            #    src=self.iframe_url,
+            #    frameborder=0,
+            #    height=0,
+            #    width=0)
+            #html.body.insert(0, iframe)
 
+            ## print tags
+            #for tag in html.find_all(True):
+            #    print(tag.name)
 
-    arguments = cgi.FieldStorage()
-    print('arg:',arguments)
-    #for i in arguments.keys():
-    #    print('<br>',arguments[i],arguments[i].value)
+            ## text replacement
+            findtoure = html.find_all(text=re.compile('HTML'))
+            for comment in findtoure:
+                fixed_text = comment.replace('HTML', 'Yaya Toure')
+                comment.replace_with(fixed_text)
 
-
-    testdir = '2'
-    indexfile = 'index.html'
-    content = fileToStr(os.path.join(testdir, indexfile))
-
-    ## change all links to python script arguments
-    content = changeLinks(content)
-
-    print(content.encode("utf-8"))
-
-
-# standard code for future cgi scripts from here on
-def fileToStr(fileName):
-    """Return a string containing the contents of the named file."""
-
-    fin = open(fileName, encoding="utf-8")
-    contents = fin.read()
-    fin.close()
-    return contents
-
-
-def changeLinks(content):
-    content = re.sub('href=\"', 'href=\"?url=', content)
-    return content
+        flow.response.content = str(html).encode("utf8")
 
 
 
-try:   # NEW
-    print("Content-type: text/html\n\n")   # say generating html
-    main()
-except:
-    cgi.print_exception()                 # catch and print errors
+def start():
+    if len(sys.argv) != 1:
+        raise ValueError('Usage: -s "antiscraper.py"')
+    return Injector()#(sys.argv[1])
